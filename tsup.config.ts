386add31ad42c764539aa1aec697bfa97c3cd64e
hanzydev <emirhanbaltas34@gmail.com>
2023-12-07 01:17:09 +0300

    better auto import

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,50 +1,5 @@
-import { readFileSync, writeFileSync } from 'fs';
 import { defineConfig } from 'tsup';
-import { createUnimport, type InlinePreset } from 'unimport';
-import AutoImport from 'unplugin-auto-import/rollup';
-
-const imports: InlinePreset[] = [
-    {
-        from: 'node:fs',
-        imports: [
-            'existsSync',
-            'readFileSync',
-            'writeFileSync',
-            'appendFileSync',
-            'renameSync',
-            'unlinkSync',
-            'statSync',
-            'readdirSync',
-            'mkdirSync',
-            'cpSync',
-            'rmSync',
-        ],
-    },
-    {
-        from: 'zod',
-        imports: ['z'],
-    },
-    {
-        from: 'fastify',
-        imports: ['FastifyRequest', 'FastifyReply'],
-        type: true,
-    },
-    {
-        from: '@types',
-        imports: [
-            'Awaitable',
-            'PCInfo',
-            'Upload',
-            'Note',
-            'Log',
-            'Code',
-            'ShortenedURL',
-            'Folder',
-            'Backup',
-        ],
-        type: true,
-    },
-];
+import AutoImport from 'unplugin-auto-import';
 
 export default defineConfig({
     target: 'esnext',
@@ -56,33 +11,81 @@ export default defineConfig({
     minify: false,
     config: 'tsconfig.json',
     plugins: [
-        AutoImport({
-            include: [/\.ts$/],
-            imports,
-            dts: 'auto-imports.d.ts',
-            injectAtEnd: true,
-            eslintrc: {
-                enabled: true,
-                filepath: '.eslintrc-auto-import.json',
-                globalsPropValue: true,
+        {
+            name: 'auto-import',
+            async buildStart() {
+                await autoImport.buildStart();
             },
-        }),
+            async buildEnd() {
+                await autoImport.buildEnd();
+            },
+            async renderChunk(code, { path }) {
+                const transformed = await autoImport.transform(code, path);
+
+                if (!transformed) {
+                    return { code };
+                }
+
+                return { code: transformed.code };
+            },
+        },
     ],
-    onSuccess: async () => {
-        const { walk } = await import('@util/walk');
-
-        const { injectImports } = createUnimport({
-            imports: imports.reduce((acc, { from, imports, ...other }) => {
-                imports.forEach((i) => acc.push({ from, name: i, ...other }));
-                return acc;
-            }, []),
-        });
-
-        for (const file of walk('./dist')) {
-            const content = readFileSync(file, 'utf-8');
-            const newContent = (await injectImports(content)).code;
-
-            writeFileSync(file, newContent);
-        }
-    },
 });
+
+const autoImport = AutoImport.raw(
+    {
+        include: [/\.ts$/],
+        imports: [
+            {
+                from: 'node:fs',
+                imports: [
+                    'existsSync',
+                    'readFileSync',
+                    'writeFileSync',
+                    'appendFileSync',
+                    'renameSync',
+                    'unlinkSync',
+                    'statSync',
+                    'readdirSync',
+                    'mkdirSync',
+                    'cpSync',
+                    'rmSync',
+                ],
+            },
+            {
+                from: 'zod',
+                imports: ['z'],
+            },
+            {
+                from: 'fastify',
+                imports: ['FastifyRequest', 'FastifyReply'],
+                type: true,
+            },
+            {
+                from: '@types',
+                imports: [
+                    'Awaitable',
+                    'PCInfo',
+                    'Upload',
+                    'Note',
+                    'Log',
+                    'Code',
+                    'ShortenedURL',
+                    'Folder',
+                    'Backup',
+                ],
+                type: true,
+            },
+        ],
+        dts: 'src/auto-imports.d.ts',
+        injectAtEnd: true,
+        eslintrc: {
+            enabled: true,
+            filepath: '.eslintrc-auto-import.json',
+            globalsPropValue: true,
+        },
+    },
+    {
+        framework: 'rollup',
+    },
+) as any;
