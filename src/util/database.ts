3019123a86c33b4c8e9bb54729c7fc68921a9150
hanzydev <emirhanbaltas34@gmail.com>
2023-12-07 17:22:23 +0300

    fix postgres port

--- src/util/database.ts
+++ src/util/database.ts
@@ -8,6 +8,7 @@ export const connectToDatabase = async () => {
 
     client = new pg.Client({
         host: process.env.POSTGRES_HOST,
+        port: +process.env.POSTGRES_PORT,
         user: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DATABASE,
