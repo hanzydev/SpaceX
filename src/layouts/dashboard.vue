f151ae19b19c0a5ab112393e3c5a7fd36905a728
hanzydev <contact@hanzy.dev>
2024-02-01 03:34:34 +0300

    fix layout error

--- src/layouts/dashboard.vue
+++ src/layouts/dashboard.vue
@@ -1,11 +1,13 @@
 <template>
-    <NuxtLayout name="auth-check">
-        <Navbar />
-        <Sidebar />
-        <section
-            class="h-[calc(100vh-70px)] w-full overflow-x-hidden bg-spacex-4 p-5 lg:ml-64 lg:w-[calc(100%-256px)]"
-        >
-            <slot />
-        </section>
-    </NuxtLayout>
+    <div>
+        <NuxtLayout name="auth-check">
+            <Navbar />
+            <Sidebar />
+            <section
+                class="h-[calc(100vh-70px)] w-full overflow-x-hidden bg-spacex-4 p-5 lg:ml-64 lg:w-[calc(100%-256px)]"
+            >
+                <slot />
+            </section>
+        </NuxtLayout>
+    </div>
 </template>
