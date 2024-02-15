e3943646d721e1c3df47eeb8010f52944a215b4e
hanzydev <contact@hanzy.dev>
2024-02-15 23:29:57 +0300

    made inner content rounded

--- src/layouts/dashboard.vue
+++ src/layouts/dashboard.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+    <div class="bg-spacex-3">
         <NuxtLayout name="auth-check">
             <Navbar />
             <Sidebar />
             <section
-                class="h-[calc(100vh-70px)] w-full overflow-x-hidden bg-spacex-4 p-5 lg:ml-64 lg:w-[calc(100%-256px)]"
+                class="h-[calc(100vh-70px)] w-full overflow-x-hidden bg-spacex-4 p-5 lg:ml-64 lg:w-[calc(100%-256px)] lg:rounded-tl-2xl"
             >
                 <slot />
             </section>
